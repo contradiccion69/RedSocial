e25adfb55f8b5b2663c62e9fc97a0e3f9f10baf6
Juan Nicolas Hernandez 2 <hernandezchavezjuannicolas@gmail.com>
2020-11-06 17:46:58 -0500

    Se soluciona los problemas de routing y se arregla los estilos el banner y se crea la seccion nuestras sedes con estilos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # Routing with Devise
   devise_for :users
-  root to: 'home#prueba'
+    # when the user within authenticate
+  unauthenticated :user do
+    devise_scope :user do
+      root 'home#start', as: :start_root
+    end
+  end
+    #When the user is authenticate
+  authenticate :user do
+    root 'home#index', as: :index_root
+  end
 end
